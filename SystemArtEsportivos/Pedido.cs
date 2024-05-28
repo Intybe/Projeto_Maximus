49d5cebe1ed44bfd199d343024195c7ce710b2e2
evelynkarina44 <evelynkarina535@gmail.com>
2024-05-28 14:04:15 -0300

    Foi tirado o focus evelyn

--- SystemArtEsportivos/Pedido.cs
+++ SystemArtEsportivos/Pedido.cs
@@ -32,6 +32,7 @@ namespace SystemArtEsportivos
 
             btnNovo.Enabled = false;
             // foi tirado o focus
+            // foi tirado o focus evelyn
         }
 
         //MÉTODO limpa campos
